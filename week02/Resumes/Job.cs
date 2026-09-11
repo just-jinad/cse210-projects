14654a09f3d7d4c9cd3ddb31e13ae29f4b00b4ca
Jinad Tope <jinadtope66@gmail.com>
2026-09-11 20:15:14 +0100

    updated

--- week02/Resumes/Job.cs
+++ week02/Resumes/Job.cs
@@ -6,8 +6,8 @@ public class Job
 {
     public string _jobTitle;
    public  string _company;
-    public string _startYear;
-    public string _endYear;
+    public int _startYear;
+    public int _endYear;
 
   
     public void Display()
--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -10,15 +10,15 @@ class Program
         Job job1 = new Job();
         job1._jobTitle = "Software Engineer";
         job1._company = "Microsoft";
-        job1._startYear = "2020";
-        job1._endYear = "2023";
+        job1._startYear = 2020;
+        job1._endYear = 2023;
 
 
         Job job2 = new Job();
         job2._jobTitle = "Web Developer";
         job2._company = "Google";
-        job2._startYear = "2018";
-        job2._endYear = "2020";
+        job2._startYear = 2018;
+        job2._endYear = 2020;
       
 
 
