189583392bb4174ce81c9be6b489069803eb3d90
Jinad Tope <jinadtope66@gmail.com>
2026-09-11 20:12:56 +0100

    updated

--- week02/Resumes/Resume.cs
+++ week02/Resumes/Resume.cs
@@ -9,9 +9,7 @@ class Resume
   
     public void Display()
     {
-        // return _UserName;
         Console.WriteLine($"Name: {_userName}");
-        //Write a loop in Resume.Display() that walks _jobList and calls each job's own Display() method — plus print the "Jobs:" header once, in the right place.
         Console.WriteLine("Jobs:");
         foreach (Job job in _jobList)
         {
