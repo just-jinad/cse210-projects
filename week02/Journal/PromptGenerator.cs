16638a151092f61af0a6b231976649d45e3cd4c0
Jinad Tope <jinadtope66@gmail.com>
2026-09-12 22:30:20 +0100

    updated

--- week02/Journal/PromptGenerator.cs
+++ week02/Journal/PromptGenerator.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 public class PromptGenerator
 {
-    private List<string> _prompts = new List<string>
+    public List<string> _prompts = new List<string>
     {
         "Who was the most interesting person I interacted with today?",
         "What was the best part of my day?",
@@ -12,7 +12,7 @@ public class PromptGenerator
         "If I had one thing I could do over today, what would it be?"
     };
 
-    private Random _random = new Random();
+    public Random _random = new Random();
 
     public string GetRandomPrompt()
     {
